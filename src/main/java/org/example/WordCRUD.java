4a47cf43e011093412567e63b06bc7910a9a6eeb
junghwan <morehey1536@gmail.com>
2023-09-12 17:38:05 +0900

    Load file function added

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -114,6 +114,28 @@ public class WordCRUD implements ICRUD{
         listAll(keyword);
     }
 
+    public void loadFile() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fname));
+            String line;
+            int count = 0;
 
+            while (true) {
+                line = br.readLine();
+                if (line == null) break;
+
+                String data[] = line.split("\\|");
+                int level = Integer.parseInt(data[0]);
+                String word = data[1];
+                String meaning = data[2];
+                list.add(new Word(0, level, word, meaning));
+                count++;
+            }
+            br.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
